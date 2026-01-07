464f75444648798056dc39ef816c4a9ffb92544d
binod01nep <bcb4314@gmail.com>
2026-01-07 22:55:10 +0530

    localStorage

---
+++ 14-localStorage/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const App = () => {
+  //localStorage.clear();
+  //sessionStorage.clear();
+  //localStorage.setItem('user','Binod')
+  //localStorage.setItem('age',18)
+  // const user=localStorage.getItem('user')
+  // const age=localStorage.getItem('age')
+  // console.log(user,age);
+ // localStorage.removeItem('age')
+// localStorage.removeItem('user')
+  // const user={
+  //   username:'Binod',
+  //   age:12,
+  //   city:'delhi'
+  // }
+
+  //localStorage.setItem('user',JSON.stringify(user))
+  // const user=JSON.parse(localStorage.getItem('user'));
+  // console.log(user);
+  
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
